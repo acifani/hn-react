9128ddb47ffb7f75a1aede7adadad3c7d48755ba
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-05-02 22:17:56 +0200

    Extract components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
+import PostList from './PostList'
+
+const App = () => (
+  <div className="App">
+    <Container text>
+      <Header as="h1">Hacker News</Header>
+      <PostList />
+    </Container>
+  </div>
+)
+
+export default App
---
+++ src/components/PostList.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Dimmer, Feed, Loader, Message } from 'semantic-ui-react'
+import Post from './Post'
+import cfg from '../config'
+
+class PostList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+      loading: false,
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchFrontpage()
+  }
+
+  fetchFrontpage = async () => {
+    try {
+      this.setState({ ...this.state, loading: true })
+      const response = await fetch(cfg.apiBaseUrl + 'news')
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      const data = await response.json()
+      this.setState({ posts: data, loading: false, error: null })
+    } catch (error) {
+      this.setState({ posts: [], loading: false, error: error.message })
+    }
+  }
+
+  render() {
+    const posts = this.state.posts.map(p => <Post post={p} />)
+
+    return (
+      <div>
+        <Message error hidden={!this.state.error}>
+          Error while fetching posts: {this.state.error}
+        </Message>
+        <Feed>{posts}</Feed>
+        <Dimmer active={this.state.loading}>
+          <Loader>Fetching posts</Loader>
+        </Dimmer>
+      </div>
+    )
+  }
+}
+
+export default PostList
