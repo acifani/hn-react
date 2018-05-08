c03141e7395f667a6c4624f7ac1d7a1ff59c7c98
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-05-08 18:26:14 +0200

    Add link to homepage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
+import { Link } from 'react-router-dom'
 import { Container, Header } from 'semantic-ui-react'
+import './App.css'
 import { CommentPage } from './Comments'
 import { NewsPage } from './News'
 
 const App = () => (
-  <div className="App">
+  <div>
     <Container text={true}>
-      <Header as="h1">Hacker News</Header>
+      <Header as="h1" className="Header">
+        <Link to="/"> Hacker News</Link>
+      </Header>
 
       <Switch>
         <Route path="/" exact={true} component={NewsPage} />
