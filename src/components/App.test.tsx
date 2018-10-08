c1f21c08b8c32c2927e6e0910a4edf27a2fe6cb6
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-10-08 23:21:01 +0200

    Fix tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { MemoryRouter, Route } from 'react-router'
 import App from './App'
 
