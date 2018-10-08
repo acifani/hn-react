d14ac4cbb9b45ccca495386521cb06661724d66a
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-10-08 22:50:23 +0200

    Add react-loadable for each route

--- src/components/Utils/index.ts
+++ src/components/Utils/index.ts
@@ -1,3 +1,4 @@
 export { default as DomainLink } from './DomainLink'
 export { default as NewsLink } from './NewsLink'
 export { default as UserLink } from './UserLink'
+export { default as Loading } from './Loading'
