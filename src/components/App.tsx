8f6dc67dda596012fdcdb331748471b6f952c77e
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-10-08 23:21:01 +0200

    Fix tests

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Loadable from 'react-loadable'
+import * as React from 'react'
+import * as Loadable from 'react-loadable'
 import { Route, Switch } from 'react-router'
 import { Link } from 'react-router-dom'
 import { Container, Divider, Header } from 'semantic-ui-react'
