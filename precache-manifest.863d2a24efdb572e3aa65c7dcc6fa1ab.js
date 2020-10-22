3a25a4c9db79841ce92f994884213808d0fb2cc2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-22 21:01:55 +0000

    Deploy acifani/yahnc to github.com/acifani/yahnc.git:gh-pages

---
+++ precache-manifest.863d2a24efdb572e3aa65c7dcc6fa1ab.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2684e917fe331f84c8e4d7b68a17ef9",
+    "url": "/yahnc/index.html"
+  },
+  {
+    "revision": "1fe1c243f68308db320c",
+    "url": "/yahnc/static/css/6.9c24cd91.chunk.css"
+  },
+  {
+    "revision": "1d6421879531ee8923cd",
+    "url": "/yahnc/static/css/main.5e394b27.chunk.css"
+  },
+  {
+    "revision": "2987293ac87e16e94d7d",
+    "url": "/yahnc/static/js/0.3e1f78cb.chunk.js"
+  },
+  {
+    "revision": "3c015471c4b6ef106605",
+    "url": "/yahnc/static/js/1.961d1e8f.chunk.js"
+  },
+  {
+    "revision": "f71455ed45a22d149a83ad49d8863776",
+    "url": "/yahnc/static/js/1.961d1e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9aae0dc6a6488217750",
+    "url": "/yahnc/static/js/2.ca597dc9.chunk.js"
+  },
+  {
+    "revision": "f71455ed45a22d149a83ad49d8863776",
+    "url": "/yahnc/static/js/2.ca597dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66ceabbadfe24f0db02",
+    "url": "/yahnc/static/js/3.56f1f246.chunk.js"
+  },
+  {
+    "revision": "1fe1c243f68308db320c",
+    "url": "/yahnc/static/js/6.ae63a646.chunk.js"
+  },
+  {
+    "revision": "d464974e9afc9fd5d698788e5c7a70a1",
+    "url": "/yahnc/static/js/6.ae63a646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c21e1019a8554a8537",
+    "url": "/yahnc/static/js/7.59e50a9f.chunk.js"
+  },
+  {
+    "revision": "f71455ed45a22d149a83ad49d8863776",
+    "url": "/yahnc/static/js/7.59e50a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8debc34e4c1517dbe968",
+    "url": "/yahnc/static/js/8.542720de.chunk.js"
+  },
+  {
+    "revision": "1d6421879531ee8923cd",
+    "url": "/yahnc/static/js/main.37f43d1c.chunk.js"
+  },
+  {
+    "revision": "d49c96ef03785f187d54",
+    "url": "/yahnc/static/js/runtime-main.b1ca3d53.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/yahnc/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/yahnc/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/yahnc/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/yahnc/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/yahnc/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/yahnc/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/yahnc/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/yahnc/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/yahnc/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/yahnc/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/yahnc/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/yahnc/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/yahnc/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/yahnc/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/yahnc/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/yahnc/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
